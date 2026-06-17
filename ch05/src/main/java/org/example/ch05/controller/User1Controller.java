677606c474ca7f043bacafe3a6ce0d273d3ecd5c
kggakm <kggakm@gmail.com>
2026-06-17 10:43:21 +0900

    add ch05 ver.2

--- ch05/src/main/java/org/example/ch05/controller/User1Controller.java
+++ ch05/src/main/java/org/example/ch05/controller/User1Controller.java
@@ -1,10 +1,18 @@
 package org.example.ch05.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.ch05.dto.User1DTO;
+import org.example.ch05.service.User1Service;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class User1Controller {
+
+    // @RequiredArgsConstructor 어노테이션으로 의존객체 주입 ⭐⭐⭐⭐⭐
+    private final User1Service service;
 
     @GetMapping("/user1/list")
     public String list(){
@@ -16,8 +24,23 @@ public class User1Controller {
         return "/user1/register";
     }
 
+    @PostMapping("/user1/register")
+    public String register(User1DTO dto){
+        System.out.println(dto);
+
+        // 등록 서비스 호출
+        service.register(dto);
+
+        return "redirect:/user1/list";
+    }
+
     @GetMapping("/user1/modify")
     public String modify(){
+        return "/user1/modify";
+    }
+
+    @PostMapping("/user1/modify")
+    public String modfiy(){
         return "/user1/modify";
     }
 }
